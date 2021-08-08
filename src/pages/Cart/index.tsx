87db13d29c8d43151e003228bb569902d42bd45e
Hudson de Oliveira Ginelli <oliveiraginelli@gmail.com>
2021-08-08 18:42:35 -0300

    finish

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import {
+  MdDelete,
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+
+import { useCart } from '../../hooks/useCart';
+import { Container, ProductTable, Total } from './styles';
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  amount: number;
+}
+
+const Cart = (): JSX.Element => {
+  const { cart, removeProduct, updateProductAmount } = useCart();
+
+  const cartFormatted = cart.map(product => ({
+    ...product,
+    priceFormatted: formatPrice(product.price),
+    subtotal: formatPrice(product.price * product.amount)
+  }))
+
+  const total =
+    formatPrice(
+      cart.reduce((sumTotal, product) => {
+        return sumTotal += product.amount * product.price;
+      }, 0)
+    )
+
+  function handleProductIncrement(product: Product) {
+    updateProductAmount({ productId: product.id, amount: product.amount + 1 });
+  }
+
+  function handleProductDecrement(product: Product) {
+    updateProductAmount({ productId: product.id, amount: product.amount - 1 });
+  }
+
+  function handleRemoveProduct(productId: number) {
+    removeProduct(productId);
+  }
+
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th aria-label="product image" />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th aria-label="delete icon" />
+          </tr>
+        </thead>
+        <tbody>
+          {cartFormatted.map((product) => {
+            return (
+              <tr data-testid="product" key={product.id}>
+                <td>
+                  <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
+                </td>
+                <td>
+                  <strong>{product.title}</strong>
+                  <span>{formatPrice(product.price)}</span>
+                </td>
+                <td>
+                  <div>
+                    <button
+                      type="button"
+                      data-testid="decrement-product"
+                      disabled={product.amount <= 1}
+                      onClick={() => handleProductDecrement(product)}
+                    >
+                      <MdRemoveCircleOutline size={20} />
+                    </button>
+                    <input
+                      type="text"
+                      data-testid="product-amount"
+                      readOnly
+                      value={product.amount}
+                    />
+                    <button
+                      type="button"
+                      data-testid="increment-product"
+                      onClick={() => handleProductIncrement(product)}
+                    >
+                      <MdAddCircleOutline size={20} />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <strong>{product.subtotal}</strong>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    data-testid="remove-product"
+                    onClick={() => handleRemoveProduct(product.id)}
+                  >
+                    <MdDelete size={20} />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>{total}</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+};
+
+export default Cart;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+import { api } from '../../services/api';
+import { formatPrice } from '../../util/format';
+import { useCart } from '../../hooks/useCart';
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+interface ProductFormatted extends Product {
+  priceFormatted: string;
+}
+
+interface CartItemsAmount {
+  [key: number]: number;
+}
+
+const Home = (): JSX.Element => {
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
+
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    // TODO
+    return sumAmount = {
+      ...sumAmount,
+      [product.id]: product.amount
+    }
+  }, {} as CartItemsAmount)
+
+  useEffect(() => {
+    async function loadProducts() {
+      // TODO
+      const response = await api.get('/products');
+      setProducts(response.data);
+    }
+
+    loadProducts();
+  }, []);
+
+  function handleAddProduct(id: number) {
+    addProduct(id);
+  }
+
+  return (
+    <ProductList>
+      {products.map((product: Product) => {
+        return (
+          <li key={product.id}>
+            <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
+            <strong>{product.title}</strong>
+            <span>{formatPrice(product.price)}</span>
+            <button
+              type="button"
+              data-testid="add-product-button"
+              onClick={() => handleAddProduct(product.id)}
+            >
+              <div data-testid="cart-product-quantity">
+                <MdAddShoppingCart size={16} color="#FFF" />
+                {cartItemsAmount[product.id] || 0}
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        );
+      })}
+    </ProductList>
+  );
+};
+
+export default Home;
